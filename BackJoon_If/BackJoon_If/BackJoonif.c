5704ef3787cc8ac5b88c300d59915b6fb3533330
KimBoris <89508080+KimBoris@users.noreply.github.com>
2022-01-04 18:11:40 +0900

    백준 if문
    완성 1330, 9498, 2753, 14681, 2884

--- BackJoon_If/BackJoon_If/BackJoonif.c
+++ BackJoon_If/BackJoon_If/BackJoonif.c
@@ -46,7 +46,7 @@ int main(void)
 	}*/
 
 	//2753
-	int A;
+	/*int A;
 	scanf_s("%d", &A);
 	if (A % 4 == 0)
 	{
@@ -62,10 +62,58 @@ int main(void)
 	else
 	{
 		printf("0\n");
-	}
+	}*/
 
 	//14681
-	//풀기
+	//int x = 0;
+	//int y = 0;
+	//scanf_s("%d %d", &x, &y);
+	//if (x > 0 && y > 0)
+	//{
+	//	printf("1");
+	//}
+	//else if (x < 0 && y> 0)
+	//{
+	//	printf("2");
+	//}
+	//else if (x < 0 && y < 0)
+	//{
+	//	printf("3");
+	//}
+	//else
+	//{
+	//	printf("4");
+	//}
+
+
+	//2884 //힘들었다
+	int Hour = 0;
+	int Min = 0;
+
+	scanf_s("%d %d", &Hour, &Min);
+
+	Min = Min - 45;
+	if (Min > 59)
+	{
+		Hour = Hour + (Min / 60);
+		Min = Min % 60;
+	}
+	else if (Min < 0)
+	{
+		Hour = Hour - 1;
+		Min = 60 + Min;
+	}
+
+	if (Hour > 23)
+	{
+		Hour = Hour % 24;
+	}
+	else if (Hour < 0)
+	{
+		Hour = 24 + Hour;
+	}
+
+	printf("%d %d", Hour, Min);
 
 	return 0;
 }
